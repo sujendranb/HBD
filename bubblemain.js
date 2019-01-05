f40e98d77caaf82e781cd274696d3a3028122c01
sujendranb <41550624+sujendranb@users.noreply.github.com>
2019-01-05 19:10:16 +0530

    Update bubblemain.js

--- bubblemain.js
+++ bubblemain.js
@@ -1,21 +1,21 @@
-var myName = "HBD KEERTHI BAYBEE..!!";
+// Define color variables:
+red = [0, 100, 63];
+orange = [40, 100, 60];
+green = [75, 100, 40];
+blue = [196, 77, 55];
+purple = [280, 50, 60];
+letterColors = [red, orange, green, blue, purple];
 
-var red = [0, 100, 63];
-var orange = [40, 100, 60];
-var green = [75, 100, 40];
-var blue = [196, 77, 55];
-var purple = [280, 50, 60];
-var letterColors = [red, orange, green, blue, purple];
+// This variable controls the smallest distance at which a mouse will 
+mouseResponseThreshold = 100;
 
-drawName(myName, letterColors);
+// This variable controls how strongly the dots will try to return to their starting position
+friction = 0.7;
 
-if(10 < 3)
-{
-    bubbleShape = 'square';
-}
-else
-{
-    bubbleShape = 'circle';
-}
+// This variable controls how much the dots will rotate when interacting
+rotationForce = 0.01;
 
+message = 'Change the physics!';
+
+drawName(message, letterColors);
 bounceBubbles();
