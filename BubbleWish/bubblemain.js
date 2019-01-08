bc819a302727e2e1e40b2bbe772466526a934902
sujendranb <41550624+sujendranb@users.noreply.github.com>
2019-01-09 02:17:31 +0530

    Add files via upload

---
+++ BubbleWish/bubblemain.js
@@ -0,0 +1,21 @@
+// Define color variables:
+red = [0, 100, 63];
+orange = [40, 100, 60];
+green = [75, 100, 40];
+blue = [196, 77, 55];
+purple = [280, 50, 60];
+letterColors = [red, orange, green, blue, purple];
+
+// This variable controls the smallest distance at which a mouse will 
+mouseResponseThreshold = 100;
+
+// This variable controls how strongly the dots will try to return to their starting position
+friction = 0.7;
+
+// This variable controls how much the dots will rotate when interacting
+rotationForce = 0.01;
+
+message1 = 'Happy Birthday keerthi!';
+
+drawName(message1, letterColors);
+bounceBubbles();
